5c117f72b9db7ddca3957002b13946699029742f
Bhargav Vankayalapati <bhargav.vankayalapati3@gmail.com>
2022-05-25 18:42:54 +0200

    Update download_pallet_dataset_and_load_weights.sh
    Script downloades the pallet-block-502 dataset and selects the C and RL perspectives and splits them into train and test sets. Prompts if the trained weights should also be loaded and loades the checkpoint of cycleGAN and weights of the classifier network based on user input.

--- download_pallet_dataset_and_load_weights.sh
+++ download_pallet_dataset_and_load_weights.sh
@@ -4,9 +4,8 @@ URL=https://zenodo.org/record/6580127/files/$FILE.zip?download=1
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/pallet-block-502/
 wget -N $URL -O $ZIP_FILE
+echo This script will download the pallet-block-502 dataset to the folder './datasets/'
 mkdir $TARGET_DIR
-unzip -j $ZIP_FILE CycleGAN\ checkpoints/* -d ./output/pallet-block-502/checkpoints/   #extract the GAN checkpoints
-unzip -j $ZIP_FILE Classifier\ model/* -d ./model_classifier_C_RL/   #extract the classifier weights
 mkdir $TARGET_DIR/trainC
 mkdir $TARGET_DIR/testC
 unzip -j $ZIP_FILE CycleGAN\ input\ \&\ output/Unfiltered/C/* -d $TARGET_DIR/trainC
@@ -15,4 +14,13 @@ mkdir $TARGET_DIR/trainRL
 mkdir $TARGET_DIR/testRL
 unzip -j $ZIP_FILE CycleGAN\ input\ \&\ output/Unfiltered/RL/* -d $TARGET_DIR/trainRL
 mv $TARGET_DIR/trainRL/cropped_5*.jpeg $TARGET_DIR/testRL
+
+while true; do
+    read -p "Do you want to load the trained weights for cycleGAN and the classifier ? [Y/n]" yn
+    case $yn in
+        [Yy]* ) unzip -j $ZIP_FILE CycleGAN\ checkpoints/* -d ./output/pallet-block-502/checkpoints/; unzip -j $ZIP_FILE Classifier\ model/* -d ./model_classifier_C_RL/ ;  break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 rm $ZIP_FILE
